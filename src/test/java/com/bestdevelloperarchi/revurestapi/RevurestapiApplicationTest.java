4dc38cf387cb6d37f804a22e02dca68ded8bffa7
Abdiooa <abdiooa45@gmail.com>
2023-07-23 01:44:13 +0300

    trying for applying container tests

--- src/test/java/com/bestdevelloperarchi/revurestapi/RevurestapiApplicationTest.java
+++ src/test/java/com/bestdevelloperarchi/revurestapi/RevurestapiApplicationTest.java
@@ -3,11 +3,11 @@ package com.bestdevelloperarchi.revurestapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-class RevurestapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+class RevurestapiApplicationTest {
+    @Test
+    void contextLoads() {
+    }
 
 }
